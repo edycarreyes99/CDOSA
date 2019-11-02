9274193ed59d8a947316c7794c16a4d5eeb28d05
Edycar Reyes <edycarreyes@gmail.com>
2019-11-02 14:16:36 -0600

    se añaden las dependencias a utilizarse en el proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import {LoginComponent} from './pages/login/login.component';
 
 
 const routes: Routes = [
-  {path: '/', component: LoginComponent},
+  {path: '', component: LoginComponent},
 ];
 
 @NgModule({
