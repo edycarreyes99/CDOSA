e0af987574f3509188bf8e71cde59144ca8929c1
Edycar Reyes <edycarreyes@gmail.com>
2019-11-02 13:56:07 -0600

    se añade el componente de la vista login al modulo de las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+// Importacion de los componentes de cada vista para cada ruta
+import {LoginComponent} from './pages/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '/', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
