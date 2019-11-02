5918e937dab6aa6caffb1a9046cf4cd9a3f8d19e
Edycar Reyes <edycarreyes@gmail.com>
2019-11-02 14:21:27 -0600

    se inyectan todos los modulos de las nuevas dependencias agregadas recientemente para el correcto funcionamiento de la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,28 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+
+// Importacion para las variables de entorno
+import {environment} from '../environments/environment';
+
+// Importacion de los modulos Custom para la aplicacion
 import {MaterialModule} from './modules/material/material.module';
 
 // Importacion de Servicios
 import {ServicioService} from './services/servicio.service';
-import { LoginComponent } from './pages/login/login.component';
+
+// Importacion de los modulos para los Toastr
+import {ToastContainerModule, ToastrModule} from 'ngx-toastr';
+
+// importaciones de los modulos de firebase a utilizarce en la app
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+
+
+import {LoginComponent} from './pages/login/login.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +36,24 @@ import { LoginComponent } from './pages/login/login.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+
+    // moodulos de los formularios
+    FormsModule,
+    ReactiveFormsModule,
+
+    // modulos de @angular/fire
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule.enablePersistence({
+      synchronizeTabs: true
+    }),
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+
+    // modulos del toast
+    ToastrModule.forRoot(),
+    ToastContainerModule,
+
+    // modulos custom
     MaterialModule
   ],
   providers: [
