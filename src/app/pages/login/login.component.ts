e091507a79ac3b17a3ca02328ce3f58ba1e0672e
Edycar Reyes <edycarreyes@gmail.com>
2019-11-02 18:49:05 -0600

    se crea el diseño para login con su codigo respectivo

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import {ServicioService} from '../../services/servicio.service';
+import {SidenavComponent} from '../../components/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+// controladores para el form de login
+  loginEmailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  loginPasswordFormControl = new FormControl('', [
+    Validators.minLength(6),
+    Validators.required
+  ]);
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    public servicio: ServicioService,
+    public sidenav: SidenavComponent
+  ) {
   }
 
+  ngOnInit() {
+    this.sidenav.sidenavAbierta = false;
+  }
+
+  // metodo para login
+  login() {
+    if (this.verificarDatosParaLogin()) {
+      this.servicio.login(this.loginEmailFormControl.value.toString(), this.loginPasswordFormControl.value.toString()).then((user) => {
+        this.servicio.newToast(1, '¡Inicio de Sesión Correcto!', `¡Bienvenido ${user.displayName}!`);
+        // this.servicio.navegar('home');
+        console.log(user.uid);
+      }).catch(e => {
+        console.error(e);
+        this.mostrarErrorAPartirDeCodigo(e.code.toString());
+        this.reiniciarControls();
+      });
+    }
+  }
+
+  // metodo para reiniciar todos los controladores
+  reiniciarControls() {
+    this.loginEmailFormControl.reset();
+    this.loginPasswordFormControl.reset();
+  }
+
+  // metodo para mostrar el error a partir del codigo de error
+  mostrarErrorAPartirDeCodigo(error: string) {
+    switch (error) {
+      // los posibles errores para login
+      case 'auth/wrong-password':
+        this.servicio.newToast(0, '¡Hubo un error!', 'La Contraeña es incorrecta.');
+        break;
+      case 'auth/network-request-failed':
+        this.servicio.newToast(0, '¡Hubo un error!', 'Revise su conexion a internet.');
+        break;
+      case 'auth/user-not-found':
+        this.servicio.newToast(0, '¡Hubo un error!', 'El usuario o correo ingresado no existen.');
+        break;
+      // los posibles errores para el signup
+      case 'auth/email-already-in-use':
+        this.servicio.newToast(0, '¡Hubo un error!', 'Este email ya esta en uso.');
+        break;
+      default:
+        this.servicio.newToast(0, '¡Hubo un error!', `${error}`);
+        break;
+    }
+  }
+
+  // metodo para verificar que todos los datos de login esten correctemente llenados
+  verificarDatosParaLogin(): boolean {
+    return this.loginEmailFormControl.valid && this.loginPasswordFormControl.valid;
+  }
 }
--- src/app/services/servicio.service.ts
+++ src/app/services/servicio.service.ts
@@ -1,9 +1,48 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+
+// importacion de los componentes del ngx-toastr
+import {ToastrService, IndividualConfig} from 'ngx-toastr';
+import {User} from 'firebase';
+import {AngularFireAuth} from '@angular/fire/auth';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioService {
 
-  constructor() { }
+  constructor(
+    public toast: ToastrService,
+    public auth: AngularFireAuth,
+  ) {
+  }
+
+  // funcion para mostrar los toast desde cualquier pagina que tenga acceso al servicio
+  public newToast(tipo: number, titulo: string, mensaje: string) {
+    console.log('Mostrando el toast');
+    const toastSettings: Partial<IndividualConfig> = {
+      timeOut: 7000,
+      closeButton: true,
+      progressBar: true,
+      progressAnimation: 'increasing'
+    };
+    if (tipo === 1) {
+      this.toast.success(`${mensaje}`, `${titulo}`, toastSettings);
+    } else {
+      this.toast.error(`${mensaje}`, `${titulo}`, toastSettings);
+    }
+  }
+
+  // funcion para realizar el login de los usuarios
+  public login(email: string, password: string): Promise<User> {
+    // tslint:disable-next-line:no-shadowed-variable
+    return new Promise<User>((resolve, reject) => {
+      this.auth.auth.signInWithEmailAndPassword(email, password)
+        .then(datosUsuario => resolve(datosUsuario.user), err => reject(err))
+        .catch((err) => {
+          console.error(err);
+        });
+    });
+  }
+
 }
