105c42be9dd162337a876880503b78f07ac92961
Edycar Reyes <edycarreyes@gmail.com>
2019-12-01 13:39:49 -0600

    Se añade la dependencia @types/reactstrap para utilizar reactstrap con typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.scss';
+import LoginPage from "./pages/login/LoginPage";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {};
+    }
+
+    render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+        return (
+            <LoginPage/>
+        );
+    }
 }
 
 export default App;
